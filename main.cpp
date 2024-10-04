e7ad157a8b864bab2472a310b67e80b3b84fc2f7
MD^ (Martin) <23630655+00Martin@users.noreply.github.com>
2024-10-04 14:42:15 +0200

    0.1.1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 /*
     Nom du projet   :   Afficher nombres premiers
     Auteur          :   Martin Derleth
-    Date            :   October 3, 2024
+    Date            :   October 4, 2024
     Nom du fichier  :   main.cpp
-    Version         :   0.1.0
+    Version         :   0.1.1
 */
 
 #include <iostream>
@@ -13,42 +13,48 @@ using namespace std;
 
 int main() {
     //Variables
-    bool restart       = true   ;
+    bool redemarrage   = true   ;
     bool entree_fausse = false  ;
-    unsigned char entree_restart;
-    unsigned int  entree_limite ;
+
+    unsigned char   entree_restart  ;
+    unsigned int    entree_limite   ;
+    unsigned int    premier_calcule ;
 
     do {
-        //Saisie utilisateur
+
+        //Saisie d'utilisateur
         do {
             cout << "Entrer une valeur       [2-1000] : ";
-            cin >> entree_limite;
+            cin  >> entree_limite;
 
-            if (entree_limite > 1 && entree_limite < 1001) entree_fausse = false;
-            else entree_fausse = true;
+            if   (entree_limite > 1 && entree_limite < 1001) entree_fausse = false  ;
+            else                                             entree_fausse = true   ;
 
         } while (entree_fausse);
 
-        //Calculs
-
 
         //Affichage
+        /*
+        //https://en.wikipedia.org/wiki/Prime_number#/media/File:Sieve_of_Eratosthenes_animation.gif
+        //for dans for
+        //Si n'est pas divisible par 2, 3, 5, 7 ou n'est pas 2, 3, 5, 7
+        */
 
 
         //Démarrage du programme à nouveau si utilisateur le désire
         do {
             entree_fausse = false;
 
-            cout << "Voulez-vous recommencer [O/N]    : ";
-            cin >> entree_restart;
+            cout << "Voulez-vous recommencer [O/N]    : "   ;
+            cin  >> entree_restart                          ;
 
-            if      (entree_restart == 'N') restart = false;
-            else if (entree_restart == 'O') restart = true ;
-            else                            entree_fausse = true;
+            if      (entree_restart == 'N') redemarrage     = false;
+            else if (entree_restart == 'O') redemarrage     = true ;
+            else                            entree_fausse   = true;
 
         } while (entree_fausse);
 
-    } while (restart);
+    } while (redemarrage);
 
     return EXIT_SUCCESS;
 }
