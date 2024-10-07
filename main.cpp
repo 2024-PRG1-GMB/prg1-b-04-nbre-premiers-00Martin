37a42ae5acfaa41732f724d629d955301f04fb20
MD^ (Martin) <23630655+00Martin@users.noreply.github.com>
2024-10-07 15:47:49 +0200

    1.0.0

--- main.cpp
+++ main.cpp
@@ -1,44 +1,64 @@
 /*
     Nom du projet   :   Afficher nombres premiers
     Auteur          :   Martin Derleth
-    Date            :   October 4, 2024
+    Date            :   October 7, 2024
     Nom du fichier  :   main.cpp
-    Version         :   0.1.1
+    Version         :   1.0.0
+    À améliorer     :   Arrêter la recherche pour un nombre lorsqu'on a dépassé la racine carrée
 */
 
+#include <cmath>
 #include <iostream>
 #include <cstdlib>
 
 using namespace std;
 
 int main() {
-    //Variables
+    //Quelques variables
     bool redemarrage   = true   ;
     bool entree_fausse = false  ;
 
     unsigned char   entree_restart  ;
-    unsigned int    entree_limite   ;
-    unsigned int    premier_calcule ;
+    unsigned int    choix_limite    ;
 
     do {
-
         //Saisie d'utilisateur
         do {
             cout << "Entrer une valeur       [2-1000] : ";
-            cin  >> entree_limite;
+            cin  >> choix_limite;
 
-            if   (entree_limite > 1 && entree_limite < 1001) entree_fausse = false  ;
-            else                                             entree_fausse = true   ;
+            if   (choix_limite > 1 && choix_limite < 1001)  entree_fausse = false  ;
+            else                                            entree_fausse = true   ;
 
         } while (entree_fausse);
 
 
         //Affichage
-        /*
-        //https://en.wikipedia.org/wiki/Prime_number#/media/File:Sieve_of_Eratosthenes_animation.gif
-        //for dans for
-        //Si n'est pas divisible par 2, 3, 5, 7 ou n'est pas 2, 3, 5, 7
-        */
+        unsigned int increment_premier = 2;
+        do {
+            for (int colonne = 0; colonne < 5;) {
+
+                for (int calcul = 2; calcul <= increment_premier; ++calcul) {
+                    //Si nombre 2, alors cas spécial
+                    if (increment_premier == 2) {
+                        cout << '\t' << "2" << " ";
+                        ++colonne;
+                    }
+                    else if (increment_premier == (calcul + 1)) {
+                        cout << '\t' << increment_premier << " ";
+                        ++colonne;
+                    }
+                    else if (increment_premier % calcul == 0) break;
+                }
+
+                ++increment_premier;
+
+                //Sortir de la boucle, au cas où aurait fini avant la fin de la ligne
+                if (increment_premier > choix_limite) break;
+            }
+                cout << endl;
+
+        } while (increment_premier <= choix_limite);
 
 
         //Démarrage du programme à nouveau si utilisateur le désire
@@ -50,7 +70,7 @@ int main() {
 
             if      (entree_restart == 'N') redemarrage     = false;
             else if (entree_restart == 'O') redemarrage     = true ;
-            else                            entree_fausse   = true;
+            else                            entree_fausse   = true ;
 
         } while (entree_fausse);
 
